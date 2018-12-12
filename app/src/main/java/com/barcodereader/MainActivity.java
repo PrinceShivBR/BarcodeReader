10480c887ecaec0abb3a0b2b2cb9dfe318631dd2
Shivu <shivappa.battur@inubesolutions.com>
2018-12-12 17:09:32 +0530

    added custom camera feature

--- app/src/main/java/com/barcodereader/MainActivity.java
+++ app/src/main/java/com/barcodereader/MainActivity.java
@@ -2,8 +2,18 @@ package com.barcodereader;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+
+import com.barcodereader.utils.FileUtils;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.nio.channels.FileChannel;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,5 +27,40 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(new Intent(MainActivity.this, ZXingScannerActivity.class));
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main_activity, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.backup:
+                copyDatabase("barcode.db");
+                break;
+        }
+        return false;
+    }
+
+    public void copyDatabase(String databaseName) {
+        try {
+            String currentDBPath = "/data/data/" + getPackageName() + "/databases/" + databaseName;
+            File file = new File(currentDBPath);
+            if (file.exists()) {
+                String rootFolder = Environment.getExternalStorageDirectory() + "/BarcodeReader/DBBackup/";
+                FileUtils.getInstance().createDirectory(rootFolder);
+                File dbFile = new File(rootFolder + databaseName);
+                FileChannel src = new FileInputStream(currentDBPath).getChannel();
+                FileChannel dst = new FileOutputStream(dbFile).getChannel();
+                dst.transferFrom(src, 0, src.size());
+                src.close();
+                dst.close();
+            }
+        } catch (Exception e) {
+
+        }
     }
 }
--- app/src/main/java/com/barcodereader/ZXingScannerActivity.java
+++ app/src/main/java/com/barcodereader/ZXingScannerActivity.java
@@ -2,24 +2,11 @@ package com.barcodereader;
 
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
-import android.os.Environment;
-import android.provider.MediaStore;
 import android.support.annotation.Nullable;
-import android.support.v4.content.FileProvider;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.widget.Toast;
-
-import com.barcodereader.utils.FileUtils;
-
-import java.io.File;
-import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 
 import me.dm7.barcodescanner.zxing.ZXingScannerView;
 
@@ -29,7 +16,6 @@ import me.dm7.barcodescanner.zxing.ZXingScannerView;
 public class ZXingScannerActivity extends AppCompatActivity implements ZXingScannerView.ResultHandler {
     private ZXingScannerView scannerView;
     static final int REQUEST_IMAGE_CAPTURE = 1000;
-    private String mCurrentPhotoPath;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -61,7 +47,7 @@ public class ZXingScannerActivity extends AppCompatActivity implements ZXingScan
     @Override
     public void handleResult(final com.google.zxing.Result result) {
         if (result != null) {
-            final Intent cameraIntent = new Intent("android.media.action.IMAGE_CAPTURE");
+            final Intent cameraIntent = new Intent(this, CameraActivity.class);
             if (cameraIntent.resolveActivity(getPackageManager()) != null) {
                 AlertDialog dialog = new AlertDialog.Builder(this)
                         .setTitle("Scan Result")
@@ -70,27 +56,8 @@ public class ZXingScannerActivity extends AppCompatActivity implements ZXingScan
                         .setPositiveButton("OK", new DialogInterface.OnClickListener() {
                             @Override
                             public void onClick(DialogInterface dialog, int which) {
-                                // Create the File where the photo should go
-                                File photoFile = null;
-                                try {
-                                    photoFile = createImageFile(result.getText());
-                                } catch (IOException ex) {
-                                    // Error occurred while creating the File
-                                    Log.i("IMAGE CAPTURE", "IOException");
-                                }
-                                // Continue only if the File was successfully created
-                                if (photoFile != null) {
-                                    Uri photoUri;
-                                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-                                        photoUri = FileProvider.getUriForFile(ZXingScannerActivity.this,
-                                                "com.barcodereader.fileprovider",
-                                                photoFile);
-                                    } else {
-                                        photoUri = Uri.fromFile(photoFile);
-                                    }
-                                    cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoUri);
-                                    startActivityForResult(cameraIntent, REQUEST_IMAGE_CAPTURE);
-                                }
+                                cameraIntent.putExtra("result", result.getText());
+                                startActivityForResult(cameraIntent, REQUEST_IMAGE_CAPTURE);
                             }
                         })
                         .create();
@@ -100,25 +67,5 @@ public class ZXingScannerActivity extends AppCompatActivity implements ZXingScan
         } else {
             Toast.makeText(this, "No information found", Toast.LENGTH_SHORT).show();
         }
-    }
-
-    /**
-     * Method to create file for capturing the image from device camera
-     *
-     * @param text scanned result text
-     * @return file
-     */
-    private File createImageFile(String text) throws IOException {
-        // Create an image file name
-        String timeStamp = new SimpleDateFormat("yyyyMMddhhmmss").format(new Date());
-        String rootFolder = Environment.getExternalStorageDirectory() + "/BarcodeReader/";
-        FileUtils.getInstance().createDirectory(rootFolder);
-        String imageFolder = rootFolder + "images/";
-        FileUtils.getInstance().createDirectory(imageFolder);
-        File image = new File(imageFolder + text + "_" + timeStamp + ".jpg");
-
-        // Save a file: path for use with ACTION_VIEW intents
-        mCurrentPhotoPath = "file:" + image.getAbsolutePath();
-        return image;
     }
 }
